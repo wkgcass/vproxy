73590bedc08d2aa98bd383f9651b21d63d41dc7e
wkgcass <wkgcass@hotmail.com>
2019-02-11 10:15:08 +0800

    support graalvm for both mac and linux via docker

---
+++ src/main/resources/nativeImageAll.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+libdir=$1
+reflectconfigpath=$2
+reflectconfig=$3
+name=$4
+
+# make MacOS image on the host system
+native-image \
+    -H:ReflectionConfigurationFiles="$reflectconfigpath/$reflectconfig" \
+    -jar "$libdir/$name.jar" \
+    "$libdir/$name-macos"
+
+# make Linux image on docker vm
+docker \
+    run \
+    -v "$reflectconfigpath:/vproxy_res" \
+    -v "$libdir:/vproxy_lib" \
+    native-image-pack \
+    \
+    /graalvm/bin/native-image \
+    -H:ReflectionConfigurationFiles="/vproxy_res/$reflectconfig" \
+    -jar "/vproxy_lib/$name.jar" \
+    "/vproxy_lib/$name-linux"
+
+# clean the container(s)
+docker rm `docker container list --all | awk '{print $1}' | tail -n +2`
