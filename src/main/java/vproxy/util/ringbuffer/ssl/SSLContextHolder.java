f538d59330c45504fadbe27e22ce61cce9388857
wkgcass <wkgcass@hotmail.com>
2019-12-30 20:33:07 +0800

    support to compare against common name

--- src/main/java/vproxy/util/ringbuffer/ssl/SSLContextHolder.java
+++ src/main/java/vproxy/util/ringbuffer/ssl/SSLContextHolder.java
@@ -24,6 +24,10 @@ public class SSLContextHolder {
 
     private static class CertHolder {
         final X509Certificate cert;
+
+        String cn;
+        boolean cnRetrieved;
+
         List<String> san;
         boolean sanRetrieved;
 
@@ -34,7 +38,7 @@ public class SSLContextHolder {
 
     private final List<Holder> holders = new ArrayList<>();
     protected final Map<String, SSLContext> quickAccess = new ConcurrentHashMap<>();
-    // quickAccess stores those SAN name without wildcard symbol to the corresponding SSLContext
+    // quickAccess stores SNI to the corresponding SSLContext
 
     public void add(SSLContext sslContext, X509Certificate[] certs) {
         holders.add(new Holder(sslContext, certs));
@@ -82,6 +86,33 @@ public class SSLContextHolder {
     }
 
     private boolean checkSNI(Holder holder, CertHolder cert, String sni) {
+        String cn;
+        if (!cert.cnRetrieved) {
+            String dn = cert.cert.getSubjectX500Principal().getName();
+            // dn result example:
+            // CN=pixiv.net,OU=Pixiv,O=Pixiv,L=Tokyo,ST=Tokyo,C=JP
+            // CN=youtube.com,OU=Youtube,O=Google,L=NY,ST=NY,C=US
+            // CN=google.com,OU=Google,O=Google,L=NY,ST=NY,C=US
+            String[] split = dn.split(",");
+            cn = null;
+            for (String s : split) {
+                if (s.startsWith("CN=")) {
+                    cn = s.substring("CN=".length());
+                    break;
+                }
+            }
+            cert.cnRetrieved = true;
+            cert.cn = cn;
+        } else {
+            cn = cert.cn;
+        }
+        if (cn != null) {
+            assert Logger.lowLevelDebug("comparing sni " + sni + " with commonName " + cn);
+            if (compare(cn, sni)) { // cn matches
+                quickAccess.put(sni, holder.sslContext);
+                return true;
+            }
+        }
         List<String> sanStrList;
         // san result example:
         // [[2, *.pixiv.net], [2, pixiv.net], [2, *.pixiv.org], [2, pixiv.org], [2, *.pximg.net], [2, pximg.net], [2, *.ads-pixiv.net], [2, ads-pixiv.net]]
@@ -121,26 +152,28 @@ public class SSLContextHolder {
         }
         for (var dnsName : sanStrList) {
             assert Logger.lowLevelDebug("comparing sni " + sni + " with dnsName " + dnsName);
-            if (dnsName.startsWith("*.")) {
-                // wildcard record
-                String suffix = dnsName.substring("*.".length());
-                if (sni.length() > suffix.length()) {
-                    if (sni.endsWith(suffix)) {
-                        String prefix = sni.substring(0, sni.length() - suffix.length() - 1 /*also remove the `dot`*/);
-                        if (!prefix.contains(".")) {
-                            quickAccess.put(dnsName, holder.sslContext);
-                            return true;
-                        }
-                    }
-                }
-            } else {
-                // plain
-                if (sni.equals(dnsName)) {
-                    quickAccess.put(dnsName, holder.sslContext);
-                    return true;
-                }
+            if (compare(dnsName, sni)) {
+                quickAccess.put(sni, holder.sslContext);
+                return true;
             }
         }
         return false;
+    }
+
+    private boolean compare(String dnsName, String sni) {
+        if (dnsName.startsWith("*.")) {
+            // wildcard record
+            String suffix = dnsName.substring("*".length());
+            if (sni.length() > suffix.length()) {
+                if (sni.endsWith(suffix)) {
+                    String prefix = sni.substring(0, sni.length() - suffix.length());
+                    return !prefix.contains(".");
+                }
+            }
+            return false;
+        } else {
+            // plain
+            return sni.equals(dnsName);
+        }
     }
 }
