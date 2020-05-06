c1e8bffd8940ef1d48207519878a019fb026fa0f
wkgcass <wkgcass@hotmail.com>
2020-05-06 20:19:29 +0800

    fix: macos tuntap driver blocks forever if no ip

--- src/main/java/vfd/posix/PosixTapDatagramFD.java
+++ src/main/java/vfd/posix/PosixTapDatagramFD.java
@@ -2,11 +2,13 @@ package vfd.posix;
 
 import vfd.TapDatagramFD;
 import vfd.TapInfo;
+import vproxy.util.OS;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 public class PosixTapDatagramFD extends PosixNetworkFD implements TapDatagramFD {
     public final TapInfo tap;
@@ -44,5 +46,27 @@ public class PosixTapDatagramFD extends PosixNetworkFD implements TapDatagramFD 
     @Override
     public TapInfo getTap() {
         return tap;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (isOpen()) {
+            if (OS.isMac()) { // hack on mac
+                // the kernel extension will block forever if the tap is not assigned with any ip
+                // so assign one for it to make sure it doesn't reach that condition
+                // BSD will definitely have /sbin/ifconfig, so it's safe to call
+                new Thread(() -> {
+                    ProcessBuilder pb = new ProcessBuilder()
+                        .command("/sbin/ifconfig", tap.dev, "0.0.0.1/32");
+                    try {
+                        var p = pb.start();
+                        p.waitFor(1, TimeUnit.SECONDS);
+                        p.destroyForcibly();
+                    } catch (Throwable ignore) {
+                    }
+                }).start();
+            }
+        }
+        super.close();
     }
 }
