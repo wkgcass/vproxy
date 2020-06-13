374c0ca0404eb6b8b852270b6e5860352f65aa98
wkgcass <wkgcass@hotmail.com>
2020-06-13 16:22:51 +0800

    1.0.0-BETA-10 release

--- base/src/main/java/vproxybase/util/Version.java
+++ base/src/main/java/vproxybase/util/Version.java
@@ -1,5 +1,5 @@
 package vproxybase.util;
 
 public class Version {
-    public static final String VERSION = "1.0.0-BETA-10-DEV"; // _THE_VERSION_
+    public static final String VERSION = "1.0.0-BETA-10"; // _THE_VERSION_
 }
