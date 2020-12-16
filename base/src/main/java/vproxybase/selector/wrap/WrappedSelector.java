193ccbbcf6cc0ba03b9df590b2dcb1184d56ef64
wkgcass <wkgcass@hotmail.com>
2020-12-17 05:35:01 +0800

    use trace for virtual fd registration warnings

--- base/src/main/java/vproxybase/selector/wrap/WrappedSelector.java
+++ base/src/main/java/vproxybase/selector/wrap/WrappedSelector.java
@@ -280,7 +280,7 @@ public class WrappedSelector implements FDSelector {
             return;
         }
         if (!virtualSocketFDs.containsKey(vfd)) {
-            Logger.warn(LogType.IMPROPER_USE, "cannot register readable for " + vfd + " when the fd not handled by this selector" +
+            Logger.trace(LogType.IMPROPER_USE, "cannot register readable for " + vfd + " when the fd not handled by this selector" +
                 " Maybe it comes from a pre-registration process. You may ignore this warning if it does not keep printing.");
             return;
         }
@@ -311,7 +311,7 @@ public class WrappedSelector implements FDSelector {
             return;
         }
         if (!virtualSocketFDs.containsKey(vfd)) {
-            Logger.warn(LogType.IMPROPER_USE, "cannot register writable for " + vfd + " when the fd not handled by this selector." +
+            Logger.trace(LogType.IMPROPER_USE, "cannot register writable for " + vfd + " when the fd not handled by this selector." +
                 " Maybe it comes from a pre-registration process. You may ignore this warning if it does not keep printing.");
             return;
         }
