27ba1bf28a122baa98418c966c72403156fb8c01
wkgcass <wkgcass@hotmail.com>
2020-12-28 11:51:38 +0800

    add annotation Comment

--- base/src/main/java/vfd/NetworkFD.java
+++ base/src/main/java/vfd/NetworkFD.java
@@ -1,11 +1,15 @@
 package vfd;
 
+import vproxybase.util.Comment;
+
 import java.io.IOException;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
 
 public interface NetworkFD<ADDR extends SockAddr> extends FD, ReadableByteChannel, WritableByteChannel {
+    @Comment("maybe null when not connected, but must be filled when connected")
     ADDR getLocalAddress() throws IOException;
 
+    @Comment("should not be null")
     ADDR getRemoteAddress() throws IOException;
 }
