3bcf67dc1099d0c0524de693c1f4449e3a0aec99
nintha <ninthakeey@hotmail.com>
2021-02-14 00:12:41 +0800

    JDKUnsafe will fallback when java.base/jdk.internal.misc is not exported

---
+++ base/src/main/java/vproxybase/util/unsafe/JDKUnsafeImpl.java
@@ -0,0 +1,11 @@
+package vproxybase.util.unsafe;
+
+import jdk.internal.misc.Unsafe;
+
+public class JDKUnsafeImpl implements JDKUnsafe{
+    private static final Unsafe unsafe = Unsafe.getUnsafe();
+
+    public byte[] allocateUninitialized0(int len) {
+        return (byte[]) unsafe.allocateUninitializedArray(byte.class, len);
+    }
+}
