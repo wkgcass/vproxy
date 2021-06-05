984d38b293a160256b9ff8e7b51f86eb051dfe69
wkgcass <wkgcass@hotmail.com>
2021-06-06 04:16:16 +0800

    1.0.0-BETA-11 release

--- base/src/main/java/vproxybase/util/Version.java
+++ base/src/main/java/vproxybase/util/Version.java
@@ -1,5 +1,5 @@
 package vproxybase.util;
 
 public class Version {
-    public static final String VERSION = "1.0.0-BETA-11-DEV"; // _THE_VERSION_
+    public static final String VERSION = "1.0.0-BETA-11"; // _THE_VERSION_
 }
