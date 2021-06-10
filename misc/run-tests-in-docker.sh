f77536a1e3a960f6698ae79be86b7250c348b646
wkgcass <wkgcass@hotmail.com>
2021-06-10 20:21:04 +0800

    fix run-tests-in-docker script

--- misc/run-tests-in-docker.sh
+++ misc/run-tests-in-docker.sh
@@ -5,7 +5,10 @@ set -e
 testclasses=`cat test/src/test/java/vproxy/test/VSuite.java | grep '\bTest' | cut -d '.' -f 1 | xargs`
 image="wkgcass/vproxy-test:latest"
 
-id=`docker run -it --rm "$image" uuid 2>/dev/null`
+docker pull "$image"
+
+id=`docker run -it --rm "$image" uuid 2>/dev/null | cut -d '-' -f 1`
+echo "random id: \`$id'"
 name="vproxy-test-$id"
 
 docker run -d -it --rm -v `pwd`:/vproxy --name "$name" "$image" /bin/bash -c 'sleep 1200s'
