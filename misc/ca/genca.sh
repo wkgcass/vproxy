17c336bda5021b90e2e4514d2369de4812e4bf01
wkgcass <wkgcass@hotmail.com>
2021-06-14 01:14:55 +0800

    genca.sh asks yes/no before actually executing

--- misc/ca/genca.sh
+++ misc/ca/genca.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+while true; do
+    read -p "Do you want to generate ca cert and key? [yes/no] " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 openssl genrsa -out ca.key 2048
 openssl req -new -x509 -extensions v3_ca -days 36500 -key ca.key -out ca.crt -config <(cat openssl.cnf v3_ca.cnf)
