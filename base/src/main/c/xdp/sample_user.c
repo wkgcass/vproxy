99d8b84cc3f3a14c9ca9fa12dad71c3b21d07449
wkgcass <wkgcass@hotmail.com>
2021-07-03 01:36:07 +0800

    add vproxy xdp c impl and sample program

---
+++ base/src/main/c/xdp/sample_user.c
@@ -0,0 +1,187 @@
+#include "vproxy_xdp.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/if_link.h>
+#include <poll.h>
+
+// Usage:
+//     hexDump(desc, addr, len, perLine);
+//         desc:    if non-NULL, printed as a description before hex dump.
+//         addr:    the address to start dumping from.
+//         len:     the number of bytes to dump.
+//         perLine: number of bytes on each output line.
+
+// hexDump is copied from https://stackoverflow.com/questions/7775991/how-to-get-hexdump-of-a-structure-data
+void hexDump (
+    const char * desc,
+    const void * addr,
+    const int len,
+    const int perLine
+) {
+    int i;
+    unsigned char buff[perLine+1];
+    const unsigned char * pc = (const unsigned char *)addr;
+
+    // Output description if given.
+
+    if (desc != NULL) printf ("%s:\n", desc);
+
+    // Length checks.
+
+    if (len == 0) {
+        printf("  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0) {
+        printf("  NEGATIVE LENGTH: %d\n", len);
+        return;
+    }
+
+    // Process every byte in the data.
+
+    for (i = 0; i < len; i++) {
+        // Multiple of perLine means new or first line (with line offset).
+
+        if ((i % perLine) == 0) {
+            // Only print previous-line ASCII buffer for lines beyond first.
+
+            if (i != 0) printf ("  %s\n", buff);
+
+            // Output the offset of current line.
+
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+
+        printf (" %02x", pc[i]);
+
+        // And buffer a printable ASCII character for later.
+
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
+            buff[i % perLine] = '.';
+        else
+            buff[i % perLine] = pc[i];
+        buff[(i % perLine) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly perLine characters.
+
+    while ((i % perLine) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    // And print the final ASCII buffer.
+
+    printf ("  %s\n", buff);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("the first argument should be ifname to attach the ebpf program\n");
+        return 1;
+    }
+    char* ifname = argv[1];
+
+    struct bpf_object* bpfobj = vp_bpfobj_attach_to_if("sample_kern.o", "xdp_sock", ifname, XDP_FLAGS_SKB_MODE);
+    if (bpfobj == NULL) {
+        return 1;
+    }
+    struct bpf_map* map = vp_bpfobj_find_map_by_name(bpfobj, "xsks_map");
+    if (map == NULL) {
+        return 1;
+    }
+
+    struct vp_umem_info* umem = vp_umem_create(5, 4, 4, XSK_UMEM__DEFAULT_FRAME_SIZE, 0);
+    if (umem == NULL) {
+        return 1;
+    }
+    struct vp_xsk_info* xsk = vp_xsk_create(ifname, 0, umem, 2048, 2048, XDP_FLAGS_SKB_MODE, XDP_COPY);
+    if (xsk == NULL) {
+        return 1;
+    }
+    int ret = vp_xsk_add_into_map(map, 0, xsk);
+    if (ret) {
+        return 1;
+    }
+
+    vp_bpfobj_release(bpfobj);
+
+    printf("ready to poll\n");
+
+    struct pollfd fds[2];
+    memset(fds, 0, sizeof(fds));
+    fds[0].fd = vp_xsk_socket_fd(xsk);
+    fds[0].events = POLLIN;
+
+    int total = 20;
+    int cnt = 0;
+    printf("this program will recieve %d packets and then exit\n", total);
+    while (1) {
+        int ret = poll(fds, 1, -1);
+        if (ret <= 0 || ret > 1) continue;
+
+        printf("poll triggered\n");
+
+        int32_t idx_rx = -1;
+        struct vp_chunk_info* chunk;
+        int rcvd = vp_xdp_fetch_pkt(xsk, &idx_rx, &chunk);
+        if (rcvd == 0) {
+            continue;
+        }
+        printf("rcvd = %d\n", rcvd);
+        for (int i = 0; i < rcvd; ++i) {
+            vp_xdp_fetch_pkt(xsk, &idx_rx, &chunk);
+            printf("received packet: cnt=%d addr=%lu pkt=%lu len=%d umem_size=%d umem_used=%d\n",
+                   ++cnt, chunk->addr, ((size_t) chunk->pkt) - ((size_t) umem->buffer), chunk->pktlen,
+                   umem->chunks.size, umem->chunks.used);
+            hexDump("received", chunk->pkt, chunk->pktlen, 16);
+
+            if (chunk->pktlen >= 12) {
+                for (int i = 0; i < 6; ++i) {
+                    char b = chunk->pkt[i];
+                    chunk->pkt[i] = chunk->pkt[6 + i];
+                    chunk->pkt[6 + i] = b;
+                }
+                if (cnt %2 == 0) {
+                    printf("echo the packet without copying\n");
+                    chunk->ref++;
+                    vp_xdp_write_pkt(xsk, chunk);
+                } else {
+                    printf("copy and echo the packet\n");
+                    struct vp_chunk_info* chunk2 = vp_chunk_fetch(&umem->chunks);
+                    if (chunk2 == NULL) {
+                        printf("ERR! umem no enough chunks: size=%d used=%d\n",
+                               umem->chunks.size, umem->chunks.used);
+                        continue;
+                    }
+                    chunk2->pktaddr = chunk2->addr;
+                    chunk2->pkt = chunk2->realaddr;
+                    chunk2->pktlen = chunk->pktlen;
+                    memcpy(chunk2->pkt, chunk->pkt, chunk->pktlen);
+
+                    vp_xdp_write_pkt(xsk, chunk2);
+                }
+            }
+
+            vp_chunk_release(&umem->chunks, chunk);
+
+            if (cnt == total) {
+                goto out_loop;
+            }
+        }
+        vp_xdp_rx_release(xsk, rcvd);
+        vp_xdp_fill_ring_fillup(umem);
+        vp_xdp_complete_tx(xsk);
+    }
+out_loop:
+    vp_xsk_close(xsk);
+    vp_umem_close(umem, true);
+
+    return 0;
+}
---
+++ base/src/main/c/xdp/vproxy_xdp.h
@@ -0,0 +1,127 @@
+#ifndef VPROXY_XDP_H
+#define VPROXY_XDP_H
+
+#include <stdint.h>
+
+#include <bpf.h>
+#include <xsk.h>
+
+struct vp_umem_info;
+struct vp_xsk_info;
+
+struct vp_chunk_info {
+    struct vp_umem_info* umem;
+    struct vp_xsk_info*  xsk;
+
+    uint8_t  ref;  // reference count, 0 means it can be used by the rings
+    uint64_t addr; // offset of this chunk
+    uint64_t endaddr; // offset of the end cursor (exclusive) of this chunk
+    char*    realaddr; // memory location
+
+    // used when receiving
+    uint64_t pktaddr;
+    char*    pkt;
+    uint32_t pktlen;
+};
+
+struct vp_chunk_array {
+    int frame_size;
+    int size;
+    int used;
+    int idx; // for iteration
+    struct vp_chunk_info* array;
+};
+
+struct vp_umem_info {
+    struct xsk_ring_prod fill_ring;
+    struct xsk_ring_cons comp_ring;
+    struct xsk_umem*     umem;
+
+    char* buffer;
+    int buffer_size;
+
+    struct vp_chunk_array chunks;
+};
+
+struct vp_xsk_info {
+    struct xsk_ring_cons rx;
+    struct xsk_ring_prod tx;
+    struct vp_umem_info* umem;
+    struct xsk_socket*   xsk;
+
+    int rx_ring_size;
+    int tx_ring_size;
+
+    int outstanding_tx;
+};
+
+struct bpf_object* vp_bpfobj_attach_to_if    (char* filepath, char* prog, char* ifname, int attach_flags);
+struct bpf_map*    vp_bpfobj_find_map_by_name(struct bpf_object* bpfobj, char* name);
+
+struct vp_umem_info* vp_umem_create(int chunks_size, int fill_ring_size, int comp_ring_size,
+                                    uint64_t frame_size, int headroom);
+struct vp_xsk_info*  vp_xsk_create (char* ifname, int queue_id, struct vp_umem_info* umem,
+                                    int rx_ring_size, int tx_ring_size, int xdp_flags, int bind_flags);
+
+int vp_xsk_add_into_map(struct bpf_map* map, int key, struct vp_xsk_info* xsk);
+
+int vp_xsk_socket_fd(struct vp_xsk_info* xsk);
+
+void vp_xsk_close (struct vp_xsk_info* xsk);
+void vp_umem_close(struct vp_umem_info* umem, bool clean_buffer);
+
+void vp_xdp_fill_ring_fillup(struct vp_umem_info* umem);
+// the first invocation to this function, idx_rx should be set to -1 (all bits 1)
+// `idx_rx` will be set in each iteration and user may not modify the value
+// return received packet count, user should iterate [return value] times
+// if idx_rx is not -1, return value will always be 0
+// otherwise, returning 0 indicates that there's no packets
+// also, when idx_rx is -1, the chunk will not be set, and packets will not be retrieved
+int  vp_xdp_fetch_pkt(struct vp_xsk_info* xsk, uint32_t* idx_rx_ptr, struct vp_chunk_info** chunkptr);
+void vp_xdp_rx_release(struct vp_xsk_info* xsk, int cnt);
+int  vp_xdp_write_pkt(struct vp_xsk_info* xsk, struct vp_chunk_info* chunk);
+void vp_xdp_complete_tx(struct vp_xsk_info* xsk);
+
+void vp_bpfobj_release(struct bpf_object* bpf_obj);
+
+
+
+static inline struct vp_chunk_info* vp_chunk_seek(struct vp_chunk_array* chunks, uint64_t addroff) {
+    return &chunks->array[addroff / chunks->frame_size];
+}
+
+static inline struct vp_chunk_info* vp_chunk_fetch(struct vp_chunk_array* chunks) {
+    if (chunks->size == chunks->used) return NULL;
+    int last = chunks->idx;
+    while (1) {
+        struct vp_chunk_info* info = &chunks->array[chunks->idx++];
+        if (chunks->idx == chunks->size) {
+            chunks->idx = 0;
+        }
+        if (info->ref == 0) {
+            info->ref = 1;
+            chunks->used++;
+            return info;
+        }
+        if (chunks->idx == last) { // runs a whole loop
+            break;
+        }
+    }
+    // should not reach here
+    fprintf(stderr, "WARN: chunks->size %d != chunks->used %d, but chunk not retrieved\n",
+            chunks->size, chunks->used);
+    return NULL;
+}
+
+static inline void vp_chunk_release(struct vp_chunk_array* chunks, struct vp_chunk_info* chunk) {
+    if (__builtin_expect(chunk->ref == 0, 0)) {
+        fprintf(stderr, "WARN: chunk ref count is 0 before trying to release it\n");
+        return;
+    }
+    if (chunk->ref == 1) {
+        chunks->used--;
+    }
+    chunk->ref--;
+}
+
+#endif
