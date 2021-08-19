24de094e375248e4596b229b207445f7d9b88c4b
wkgcass <wkgcass@hotmail.com>
2021-08-19 12:21:58 +0800

    remove useless ebpf program and add new program

--- base/src/main/c/xdp/sample_kern.c
+++ base/src/main/c/xdp/sample_kern.c
@@ -3,12 +3,12 @@
 
 struct bpf_map_def SEC("maps") xsks_map = {
     .type = BPF_MAP_TYPE_XSKMAP,
-    .max_entries = 1,
+    .max_entries = 128,
     .key_size = sizeof(int),
     .value_size = sizeof(int)
 };
 
 SEC("xdp_sock") int xdp_sock_prog(struct xdp_md *ctx)
 {
-    return bpf_redirect_map(&xsks_map, 0, XDP_DROP);
+    return bpf_redirect_map(&xsks_map, ctx->rx_queue_index, XDP_DROP);
 }
--- base/src/main/java/vproxy/xdp/BPFObject.java
+++ base/src/main/java/vproxy/xdp/BPFObject.java
@@ -222,22 +222,23 @@ public class BPFObject {
      *     .value_size = sizeof(int)
      * };
      *
-     * #define ETHER_TYPE_OFF (12)
-     * #define IP4_OFF        (14)
-     * #define IP4_IHL_OFF    (IP4_OFF)
-     * #define IP4_PROTO_OFF  (IP4_OFF  + 9)
-     * #define TCP4_OFF       (IP4_OFF  + 20)
-     * #define TCP4_DST_OFF   (TCP4_OFF + 2)
-     * #define IP6_OFF        (14)
-     * #define IP6_PROTO_OFF  (IP6_OFF  + 6)
-     * #define TCP6_OFF       (IP6_OFF  + 40)
-     * #define TCP6_DST_OFF   (TCP6_OFF + 2)
+     * #define ETHER_TYPE_OFF  (12)
+     * #define IP4_OFF         (14)
+     * #define IP4_IHL_OFF     (IP4_OFF)
+     * #define IP4_PROTO_OFF   (IP4_OFF  + 9)
+     * #define TCPUDP4_OFF     (IP4_OFF  + 20)
+     * #define TCPUDP4_DST_OFF (TCPUDP4_OFF + 2)
+     * #define IP6_OFF         (14)
+     * #define IP6_PROTO_OFF   (IP6_OFF  + 6)
+     * #define TCPUDP6_OFF     (IP6_OFF  + 40)
+     * #define TCPUDP6_DST_OFF (TCPUDP6_OFF + 2)
      *
      * #define ETHER_TYPE_IPv4 (0x0800)
      * #define ETHER_TYPE_IPv6 (0x86dd)
      * #define IP_PROTO_TCP    (6)
+     * #define IP_PROTO_UDP    (17)
      *
-     * #define HANDLED_TCP_PORT (1111)
+     * #define HANDLED_PORT (1111)
      *
      * SEC("xdp_sock") int xdp_sock_prog(struct xdp_md *ctx)
      * {
@@ -263,15 +264,15 @@ public class BPFObject {
      *             return XDP_PASS;
      *         }
      *         int proto = data[IP4_PROTO_OFF] &amp; 0xff;
-     *         if (proto != IP_PROTO_TCP) {
+     *         if (proto != IP_PROTO_TCP &amp;&amp; proto != IP_PROTO_UDP) {
      *             return XDP_PASS;
      *         }
-     *         pos = data + TCP4_DST_OFF + 2;
+     *         pos = data + TCPUDP4_DST_OFF + 2;
      *         if (pos &gt; data_end) {
      *             return XDP_PASS;
      *         }
-     *         int dst = ((data[TCP4_DST_OFF] &amp; 0xff) &lt;&lt; 8) | (data[TCP4_DST_OFF + 1] &amp; 0xff);
-     *         if (dst != HANDLED_TCP_PORT) {
+     *         int dst = ((data[TCPUDP4_DST_OFF] &amp; 0xff) &lt;&lt; 8) | (data[TCPUDP4_DST_OFF + 1] &amp; 0xff);
+     *         if (dst != HANDLED_PORT) {
      *             return XDP_PASS;
      *         }
      *     } else if (ether_type == ETHER_TYPE_IPv6) {
@@ -280,15 +281,15 @@ public class BPFObject {
      *             return XDP_PASS;
      *         }
      *         int proto = data[IP6_PROTO_OFF] &amp; 0xff;
-     *         if (proto != IP_PROTO_TCP) {
+     *         if (proto != IP_PROTO_TCP &amp;&amp; proto != IP_PROTO_UDP) {
      *             return XDP_PASS;
      *         }
-     *         pos = data + TCP6_DST_OFF + 2;
+     *         pos = data + TCPUDP6_DST_OFF + 2;
      *         if (pos &gt; data_end) {
      *             return XDP_PASS;
      *         }
-     *         int dst = ((data[TCP6_DST_OFF] &amp; 0xff) &lt;&lt; 8) | (data[TCP6_DST_OFF + 1] &amp; 0xff);
-     *         if (dst != HANDLED_TCP_PORT) {
+     *         int dst = ((data[TCPUDP6_DST_OFF] &amp; 0xff) &lt;&lt; 8) | (data[TCPUDP6_DST_OFF + 1] &amp; 0xff);
+     *         if (dst != HANDLED_PORT) {
      *             return XDP_PASS;
      *         }
      *     } else {
@@ -298,32 +299,33 @@ public class BPFObject {
      * }
      * </pre>
      */
-    private static final String SKIP_TCP_DST_PORT_EBPF_OBJECT = "" +
+    private static final String HANDLE_DST_PORT_EBPF_OBJECT = "" +
         "7f454c46020101000000000000000000" +
         "0100f700010000000000000000000000" +
-        "00000000000000001803000000000000" +
+        "00000000000000006803000000000000" +
         "00000000400000000000400007000100" +
         "b7000000020000006113040000000000" +
         "6112000000000000bf24000000000000" +
-        "070400000e0000002d342d0000000000" +
+        "070400000e0000002d342f0000000000" +
         "71250d000000000071240c0000000000" +
         "67040000080000004f54000000000000" +
-        "57040000ffff000015041500dd860000" +
-        "5504260000080000bf24000000000000" +
-        "070400000f0000002d34230000000000" +
+        "57040000ffff000015041600dd860000" +
+        "5504280000080000bf24000000000000" +
+        "070400000f0000002d34250000000000" +
         "bf240000000000000704000018000000" +
-        "2d3420000000000071240e0000000000" +
-        "570400000f00000055041d0005000000" +
-        "bf240000000000000704000026000000" +
-        "2d341a00000000007123170000000000" +
-        "55031800060000007123250000000000" +
-        "71222400000000006702000008000000" +
-        "4f3200000000000015020e00llhh0000" +
-        "0500120000000000bf24000000000000" +
-        "07040000150000002d340f0000000000" +
+        "2d3422000000000071240e0000000000" +
+        "570400000f00000055041f0005000000" +
+        "71241700000000001504010011000000" +
+        "55041c0006000000bf24000000000000" +
+        "07040000260000002d34190000000000" +
+        "71232500000000007122240000000000" +
+        "67020000080000004f32000000000000" +
+        "15020f00llhh00000500130000000000" +
+        "bf240000000000000704000015000000" +
+        "2d341000000000007124140000000000" +
+        "150401001100000055040d0006000000" +
         "bf24000000000000070400003a000000" +
-        "2d340c00000000007123140000000000" +
-        "55030a00060000007123390000000000" +
+        "2d340a00000000007123390000000000" +
         "71223800000000006702000008000000" +
         "4f3200000000000055020500llhh0000" +
         "61121000000000001801000000000000" +
@@ -333,28 +335,32 @@ public class BPFObject {
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "300000000400f1ff0000000000000000" +
+        "00000000000000006d00000000000300" +
+        "10010000000000000000000000000000" +
+        "65000000000003004001000000000000" +
         "00000000000000005d00000000000300" +
-        "70010000000000000000000000000000" +
-        "55000000000003009801000000000000" +
+        "80010000000000000000000000000000" +
+        "5500000000000300a801000000000000" +
         "00000000000000004e00000000000300" +
-        "08010000000000000000000000000000" +
+        "c8000000000000000000000000000000" +
         "22000000120003000000000000000000" +
-        "a0010000000000000c00000011000500" +
+        "b0010000000000000c00000011000500" +
         "00000000000000001400000000000000" +
-        "78010000000000000100000006000000" +
+        "88010000000000000100000008000000" +
         "002e74657874006d6170730078736b73" +
         "5f6d6170002e72656c7864705f736f63" +
         "6b007864705f736f636b5f70726f6700" +
         "73616d706c655f6b65726e2e63002e73" +
         "7472746162002e73796d746162004c42" +
-        "42305f39004c4242305f3134004c4242" +
-        "305f3133000000000000000000000000" +
+        "42305f38004c4242305f3136004c4242" +
+        "305f3135004c4242305f3133004c4242" +
+        "305f3130000000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000003e00000003000000" +
         "00000000000000000000000000000000" +
-        "b0020000000000006500000000000000" +
+        "f0020000000000007500000000000000" +
         "00000000000000000100000000000000" +
         "00000000000000000100000001000000" +
         "06000000000000000000000000000000" +
@@ -362,24 +368,24 @@ public class BPFObject {
         "00000000000000000400000000000000" +
         "00000000000000001900000001000000" +
         "06000000000000000000000000000000" +
-        "4000000000000000a001000000000000" +
+        "4000000000000000b001000000000000" +
         "00000000000000000800000000000000" +
         "00000000000000001500000009000000" +
         "00000000000000000000000000000000" +
-        "a0020000000000001000000000000000" +
+        "e0020000000000001000000000000000" +
         "06000000030000000800000000000000" +
         "10000000000000000700000001000000" +
         "03000000000000000000000000000000" +
-        "e0010000000000001400000000000000" +
+        "f0010000000000001400000000000000" +
         "00000000000000000400000000000000" +
         "00000000000000004600000002000000" +
         "00000000000000000000000000000000" +
-        "f801000000000000a800000000000000" +
-        "01000000050000000800000000000000" +
+        "0802000000000000d800000000000000" +
+        "01000000070000000800000000000000" +
         "1800000000000000";
 
-    public static ByteArray skipTcpDstPortProgram(int port) {
-        String ebpfHex = SKIP_TCP_DST_PORT_EBPF_OBJECT
+    public static ByteArray handleDstPortProgram(int port) {
+        String ebpfHex = HANDLE_DST_PORT_EBPF_OBJECT
             .replace("ll", toHexStr((byte) (port & 0xff)))
             .replace("hh", toHexStr((byte) ((port >> 8) & 0xff)));
         return ByteArray.fromHexString(ebpfHex);
@@ -387,8 +393,8 @@ public class BPFObject {
 
     /**
      * <pre>
-     * #include <linux/bpf.h>
-     * #include <bpf_helpers.h>
+     * #include &lt;linux/bpf.h&gt;
+     * #include &lt;bpf_helpers.h&gt;
      *
      * struct bpf_map_def SEC("maps") xsks_map = {
      *     .type = BPF_MAP_TYPE_XSKMAP,
@@ -397,139 +403,40 @@ public class BPFObject {
      *     .value_size = sizeof(int)
      * };
      *
-     * #define ETHER_TYPE_OFF (12)
-     * #define IP4_OFF        (14)
-     * #define IP4_IHL_OFF    (IP4_OFF)
-     * #define IP4_PROTO_OFF  (IP4_OFF  + 9)
-     * #define TCP4_OFF       (IP4_OFF  + 20)
-     * #define TCP4_SRC_OFF   (TCP4_OFF)
-     * #define IP6_OFF        (14)
-     * #define IP6_PROTO_OFF  (IP6_OFF  + 6)
-     * #define TCP6_OFF       (IP6_OFF  + 40)
-     * #define TCP6_SRC_OFF   (TCP6_OFF)
-     *
-     * #define ETHER_TYPE_IPv4 (0x0800)
-     * #define ETHER_TYPE_IPv6 (0x86dd)
-     * #define IP_PROTO_TCP    (6)
-     *
-     * #define HANDLED_TCP_PORT (1111)
-     *
      * SEC("xdp_sock") int xdp_sock_prog(struct xdp_md *ctx)
      * {
-     *     unsigned char* data_end = (unsigned char*) ((long) ctx->data_end);
-     *     unsigned char* data     = (unsigned char*) ((long) ctx->data);
-     *     unsigned char* pos = data;
-     *     pos += ETHER_TYPE_OFF + 2;
-     *     if (pos > data_end) {
-     *         return XDP_PASS;
-     *     }
-     *     int ether_type = ((data[ETHER_TYPE_OFF] & 0xff) << 8) | (data[ETHER_TYPE_OFF + 1] & 0xff);
-     *     if (ether_type == ETHER_TYPE_IPv4) {
-     *         pos = data + IP4_IHL_OFF + 1;
-     *         if (pos > data_end) {
-     *             return XDP_PASS;
-     *         }
-     *         int ip_len = data[IP4_IHL_OFF] & 0xf;
-     *         if (ip_len != 5) {
-     *             return XDP_PASS;
-     *         }
-     *         pos = data + IP4_PROTO_OFF + 1;
-     *         if (pos > data_end) {
-     *             return XDP_PASS;
-     *         }
-     *         int proto = data[IP4_PROTO_OFF] & 0xff;
-     *         if (proto != IP_PROTO_TCP) {
-     *             return XDP_PASS;
-     *         }
-     *         pos = data + TCP4_SRC_OFF + 2;
-     *         if (pos > data_end) {
-     *             return XDP_PASS;
-     *         }
-     *         int dst = ((data[TCP4_SRC_OFF] & 0xff) << 8) | (data[TCP4_SRC_OFF + 1] & 0xff);
-     *         if (dst != HANDLED_TCP_PORT) {
-     *             return XDP_PASS;
-     *         }
-     *     } else if (ether_type == ETHER_TYPE_IPv6) {
-     *         pos = data + IP6_PROTO_OFF + 1;
-     *         if (pos > data_end) {
-     *             return XDP_PASS;
-     *         }
-     *         int proto = data[IP6_PROTO_OFF] & 0xff;
-     *         if (proto != IP_PROTO_TCP) {
-     *             return XDP_PASS;
-     *         }
-     *         pos = data + TCP6_SRC_OFF + 2;
-     *         if (pos > data_end) {
-     *             return XDP_PASS;
-     *         }
-     *         int dst = ((data[TCP6_SRC_OFF] & 0xff) << 8) | (data[TCP6_SRC_OFF + 1] & 0xff);
-     *         if (dst != HANDLED_TCP_PORT) {
-     *             return XDP_PASS;
-     *         }
-     *     } else {
-     *         return XDP_PASS;
-     *     }
-     *     return bpf_redirect_map(&xsks_map, ctx->rx_queue_index, XDP_PASS);
+     *     return bpf_redirect_map(&xsks_map, ctx-&gt;rx_queue_index, XDP_DROP);
      * }
      * </pre>
      */
-    private static final String SKIP_TCP_SRC_PORT_EBPF_OBJECT = "" +
+    private static final ByteArray HANDLE_ALL_EBPF_OBJECT = ByteArray.fromHexString("" +
         "7f454c46020101000000000000000000" +
         "0100f700010000000000000000000000" +
-        "00000000000000001803000000000000" +
+        "00000000000000004801000000000000" +
         "00000000400000000000400007000100" +
-        "b7000000020000006113040000000000" +
-        "6112000000000000bf24000000000000" +
-        "070400000e0000002d342d0000000000" +
-        "71250d000000000071240c0000000000" +
-        "67040000080000004f54000000000000" +
-        "57040000ffff000015041500dd860000" +
-        "5504260000080000bf24000000000000" +
-        "070400000f0000002d34230000000000" +
-        "bf240000000000000704000018000000" +
-        "2d3420000000000071240e0000000000" +
-        "570400000f00000055041d0005000000" +
-        "bf240000000000000704000024000000" +
-        "2d341a00000000007123170000000000" +
-        "55031800060000007123230000000000" +
-        "71222200000000006702000008000000" +
-        "4f3200000000000015020e00llhh0000" +
-        "0500120000000000bf24000000000000" +
-        "07040000150000002d340f0000000000" +
-        "bf240000000000000704000038000000" +
-        "2d340c00000000007123140000000000" +
-        "55030a00060000007123370000000000" +
-        "71223600000000006702000008000000" +
-        "4f3200000000000055020500llhh0000" +
         "61121000000000001801000000000000" +
-        "0000000000000000b703000002000000" +
+        "0000000000000000b703000001000000" +
         "85000000330000009500000000000000" +
         "11000000040000000400000080000000" +
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "300000000400f1ff0000000000000000" +
-        "00000000000000005d00000000000300" +
-        "70010000000000000000000000000000" +
-        "55000000000003009801000000000000" +
-        "00000000000000004e00000000000300" +
-        "08010000000000000000000000000000" +
-        "22000000120003000000000000000000" +
-        "a0010000000000000c00000011000500" +
-        "00000000000000001400000000000000" +
-        "78010000000000000100000006000000" +
-        "002e74657874006d6170730078736b73" +
-        "5f6d6170002e72656c7864705f736f63" +
-        "6b007864705f736f636b5f70726f6700" +
-        "73616d706c655f6b65726e2e63002e73" +
-        "7472746162002e73796d746162004c42" +
-        "42305f39004c4242305f3134004c4242" +
-        "305f3133000000000000000000000000" +
+        "00000000000000002200000012000300" +
+        "00000000000000003000000000000000" +
+        "0c000000110005000000000000000000" +
+        "14000000000000000800000000000000" +
+        "0100000003000000002e74657874006d" +
+        "6170730078736b735f6d6170002e7265" +
+        "6c7864705f736f636b007864705f736f" +
+        "636b5f70726f670073616d706c655f6b" +
+        "65726e2e63002e737472746162002e73" +
+        "796d7461620000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000000000000000000000" +
         "00000000000000003e00000003000000" +
         "00000000000000000000000000000000" +
-        "b0020000000000006500000000000000" +
+        "f8000000000000004e00000000000000" +
         "00000000000000000100000000000000" +
         "00000000000000000100000001000000" +
         "06000000000000000000000000000000" +
@@ -537,27 +444,24 @@ public class BPFObject {
         "00000000000000000400000000000000" +
         "00000000000000001900000001000000" +
         "06000000000000000000000000000000" +
-        "4000000000000000a001000000000000" +
+        "40000000000000003000000000000000" +
         "00000000000000000800000000000000" +
         "00000000000000001500000009000000" +
         "00000000000000000000000000000000" +
-        "a0020000000000001000000000000000" +
+        "e8000000000000001000000000000000" +
         "06000000030000000800000000000000" +
         "10000000000000000700000001000000" +
         "03000000000000000000000000000000" +
-        "e0010000000000001400000000000000" +
+        "70000000000000001400000000000000" +
         "00000000000000000400000000000000" +
         "00000000000000004600000002000000" +
         "00000000000000000000000000000000" +
-        "f801000000000000a800000000000000" +
-        "01000000050000000800000000000000" +
-        "1800000000000000";
+        "88000000000000006000000000000000" +
+        "01000000020000000800000000000000" +
+        "1800000000000000").persist();
 
-    public static ByteArray skipTcpSrcPortProgram(int port) {
-        String ebpfHex = SKIP_TCP_SRC_PORT_EBPF_OBJECT
-            .replace("ll", toHexStr((byte) (port & 0xff)))
-            .replace("hh", toHexStr((byte) ((port >> 8) & 0xff)));
-        return ByteArray.fromHexString(ebpfHex);
+    public static ByteArray handleAllProgram() {
+        return HANDLE_ALL_EBPF_OBJECT;
     }
 
     private static String toHexStr(byte b) {
