01e31422494a62e7e8c22dee6de9b8bb2e90e3eb
wkgcass <wkgcass@hotmail.com>
2021-10-21 00:49:07 +0800

    use java 17 to compile and support maven repo

--- base/src/main/java/io/vproxy/base/util/Version.java
+++ base/src/main/java/io/vproxy/base/util/Version.java
@@ -1,5 +1,5 @@
 package io.vproxy.base.util;
 
 public class Version {
-    public static final String VERSION = "1.0.0-BETA-12-dev"; // _THE_VERSION_
+    public static final String VERSION = "1.0.0-BETA-12-DEV"; // _THE_VERSION_
 }
