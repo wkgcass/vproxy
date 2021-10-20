87a1c0065cca1ea787249e6314d26f96bee08b87
wkgcass <wkgcass@hotmail.com>
2021-10-21 01:27:16 +0800

    add io.vproxy.all to import all modules

---
+++ all/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.vproxy.all {
+    requires io.vproxy.dep;
+    requires io.vproxy.base;
+    requires io.vproxy.lib;
+    requires io.vproxy.core;
+    requires io.vproxy.extended;
+    requires io.vproxy.app;
+}
