6c1adecc44661332e31e43c56ce5d90ab7328341
wkgcass <wkgcass@hotmail.com>
2021-11-01 20:37:14 +0800

    use pointers instead of memcpy in PacketFullTuple

--- base/src/main/java/io/vproxy/vpacket/tuples/PacketFullTuple.java
+++ base/src/main/java/io/vproxy/vpacket/tuples/PacketFullTuple.java
@@ -1,63 +1,48 @@
 package io.vproxy.vpacket.tuples;
 
-import io.vproxy.base.util.ByteArray;
-import io.vproxy.base.util.Consts;
-import io.vproxy.base.util.Utils;
 import io.vproxy.vfd.IP;
-import io.vproxy.vfd.IPv4;
 import io.vproxy.vfd.MacAddress;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 public class PacketFullTuple {
-    private final byte[] data;
+    private final int devin;
+    private final MacAddress dlSrc;
+    private final MacAddress dlDst;
+    private final IP nwSrc;
+    private final IP nwDst;
+    private final int ipProto;
+    private final int tpSrc;
+    private final int tpDst;
+
     private final int hashCode;
 
-    @SuppressWarnings("UnusedAssignment")
     public PacketFullTuple(int devin, MacAddress dlSrc, MacAddress dlDst, IP nwSrc, IP nwDst, int ipProto, int tpSrc, int tpDst) {
-        byte[] data;
-        if (nwSrc instanceof IPv4) {
-            data = Utils.allocateByteArray(4 + 6 + 6 + 2 + 4 + 4 + 2 + 2 + 2);
-        } else {
-            data = Utils.allocateByteArray(4 + 6 + 6 + 2 + 16 + 16 + 2 + 2 + 2);
-        }
-        int offset = 0;
-        data[offset++] = (byte) ((devin >> 24) & 0xff);
-        data[offset++] = (byte) ((devin >> 16) & 0xff);
-        data[offset++] = (byte) ((devin >> 8) & 0xff);
-        data[offset++] = (byte) (devin & 0xff);
-        System.arraycopy(dlSrc.bytes.toJavaArray(), 0, data, offset, 6);
-        offset += 6;
-        System.arraycopy(dlDst.bytes.toJavaArray(), 0, data, offset, 6);
-        offset += 6;
-        int etherType = nwSrc instanceof IPv4 ? Consts.ETHER_TYPE_IPv4 : Consts.ETHER_TYPE_IPv6;
-        data[offset++] = (byte) ((etherType >> 8) & 0xff);
-        data[offset++] = (byte) (etherType & 0xff);
-        if (nwSrc instanceof IPv4) {
-            System.arraycopy(nwSrc.bytes.toJavaArray(), 0, data, offset, 4);
-            offset += 4;
-            System.arraycopy(nwDst.bytes.toJavaArray(), 0, data, offset, 4);
-            offset += 4;
-        } else {
-            System.arraycopy(nwSrc.bytes.toJavaArray(), 0, data, offset, 16);
-            offset += 16;
-            System.arraycopy(nwDst.bytes.toJavaArray(), 0, data, offset, 16);
-            offset += 16;
-        }
-        data[offset++] = (byte) ((ipProto >> 8) & 0xff);
-        data[offset++] = (byte) (ipProto & 0xff);
-        data[offset++] = (byte) ((tpSrc >> 8) & 0xff);
-        data[offset++] = (byte) (tpSrc & 0xff);
-        data[offset++] = (byte) ((tpDst >> 8) & 0xff);
-        data[offset++] = (byte) (tpDst & 0xff);
+        this.devin = devin;
+        this.dlSrc = dlSrc;
+        this.dlDst = dlDst;
+        this.nwSrc = nwSrc;
+        this.nwDst = nwDst;
+        this.ipProto = ipProto;
+        this.tpSrc = tpSrc;
+        this.tpDst = tpDst;
 
-        this.data = data;
-        this.hashCode = Arrays.hashCode(data);
+        this.hashCode = hashCode();
     }
 
     @Override
     public String toString() {
-        return "PacketFullTuple:" + ByteArray.from(data).toHexString();
+        return "PacketFullTuple{" +
+            "devin=" + devin +
+            ", dlSrc=" + dlSrc +
+            ", dlDst=" + dlDst +
+            ", nwSrc=" + nwSrc +
+            ", nwDst=" + nwDst +
+            ", ipProto=" + ipProto +
+            ", tpSrc=" + tpSrc +
+            ", tpDst=" + tpDst +
+            ", hashCode=" + hashCode +
+            '}';
     }
 
     @Override
@@ -65,11 +50,11 @@ public class PacketFullTuple {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PacketFullTuple that = (PacketFullTuple) o;
-        return Arrays.equals(data, that.data);
+        return devin == that.devin && ipProto == that.ipProto && tpSrc == that.tpSrc && tpDst == that.tpDst && hashCode == that.hashCode && Objects.equals(dlSrc, that.dlSrc) && Objects.equals(dlDst, that.dlDst) && Objects.equals(nwSrc, that.nwSrc) && Objects.equals(nwDst, that.nwDst);
     }
 
     @Override
     public int hashCode() {
-        return hashCode;
+        return Objects.hash(devin, dlSrc, dlDst, nwSrc, nwDst, ipProto, tpSrc, tpDst, hashCode);
     }
 }
