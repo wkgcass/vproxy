82b3c3788b87813b028c8e1799b5eb62434c3d8a
wkgcass <wkgcass@hotmail.com>
2023-05-09 15:49:53 +0800

    start 1.0.0-BETA-13-DEV

--- base/src/main/java/io/vproxy/base/util/Version.java
+++ base/src/main/java/io/vproxy/base/util/Version.java
@@ -1,5 +1,5 @@
 package io.vproxy.base.util;
 
 public class Version {
-    public static final String VERSION = "1.0.0-BETA-12"; // _THE_VERSION_
+    public static final String VERSION = "1.0.0-BETA-13-DEV"; // _THE_VERSION_
 }
