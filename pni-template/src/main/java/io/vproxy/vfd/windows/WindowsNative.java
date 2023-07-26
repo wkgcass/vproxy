02034209a77c2ad554cb712848f659a0f88d991a
wkgcass <wkgcass@hotmail.com>
2023-07-26 16:13:33 +0800

    [pni] add windows native template classes

---
+++ pni-template/src/main/java/io/vproxy/vfd/windows/WindowsNative.java
@@ -0,0 +1,24 @@
+package io.vproxy.vfd.windows;
+
+import io.vproxy.pni.annotation.Function;
+import io.vproxy.pni.annotation.Raw;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+@Function
+interface PNIWindowsNative {
+    boolean tapNonBlockingSupported() throws IOException;
+
+    long allocateOverlapped() throws IOException;
+
+    void releaseOverlapped(long overlapped) throws IOException;
+
+    long createTapHandle(String dev) throws IOException;
+
+    void closeHandle(long fd) throws IOException;
+
+    int read(long handle, @Raw ByteBuffer directBuffer, int off, int len, long overlapped) throws IOException;
+
+    int write(long handle, @Raw ByteBuffer directBuffer, int off, int len, long overlapped) throws IOException;
+}
