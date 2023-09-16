946c36db86e2a382ca33af22df80946ded4f74e7
wkgcass <wkgcass@hotmail.com>
2023-09-16 23:21:13 +0800

    [msquic] add msquic-dummy module

---
+++ msquic-dummy/src/main/java/io/vproxy/msquic/dummy/__placeholder__.java
@@ -0,0 +1,5 @@
+package io.vproxy.msquic.dummy;
+
+// add an empty class here just to make tools happy
+public class __placeholder__ {
+}
---
+++ msquic-dummy/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.vproxy.msquic {
+    requires io.vproxy.pni;
+    requires io.vproxy.base;
+
+    exports io.vproxy.msquic.dummy;
+}
