e0fd1874536bfdd9a7f8be1cbd6164c70ae29774
K.G. Wang <wkgcass@hotmail.com>
2024-07-20 11:20:25 +0800

    tmp

--- misc/init.sh
+++ misc/init.sh
@@ -12,7 +12,7 @@ if [[ ! -d ./msquic ]]; then
 else
 	refs=`cat msquic/.git/HEAD | awk '{print $2}'`
 	commit=`cat "msquic/.git/$refs"`
-	if [[ "$commit" != "8b816804c1b82ac1ca1c50dec7195113440d86ab" ]]; then
+	if [[ "$commit" != "56cafbd0c26e1cb4e169b1233fe6d620218f6f61" ]]; then
 		rm -rf ./msquic
 		git clone https://github.com/wkgcass/msquic --branch=$MSQUIC_VERSION-modified --depth=1
 	fi
